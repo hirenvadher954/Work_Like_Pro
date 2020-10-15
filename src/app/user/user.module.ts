251fc235e499525df8a733328f9031a32baf786c
Hiren Vadher <hirenvadher954@gmail.com>
2020-10-15 23:50:07 +0530

    added signIn using email and password

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,3 +1,5 @@
+import {ReactiveFormsModule} from '@angular/forms';
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -5,14 +7,16 @@ import { UserRoutingModule } from './user-routing.module';
 import { LoginComponent } from './login/login.component';
 import { GoogleSigninDirective } from './google-signin.directive';
 import {SharedModule} from '../shared/shared.module';
+import { EmailComponent } from './email/email.component';
 
 
 @NgModule({
-  declarations: [LoginComponent, GoogleSigninDirective],
+  declarations: [LoginComponent, GoogleSigninDirective, EmailComponent],
   imports: [
     CommonModule,
     UserRoutingModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule,
   ]
 })
 export class UserModule { }
